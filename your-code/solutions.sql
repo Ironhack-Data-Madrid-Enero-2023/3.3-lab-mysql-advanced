d5d4cfa31a068426999c4001802aaa2535bb9c9e
Jorge Mendoza <jmgpgit@gmail.com>
2023-02-01 17:19:21 +0100

    finish

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -2,10 +2,10 @@
 
 select Author_ID, Profit
 from
-(select author_id as Author_ID, concat(a_s.first_name,' ',a_s.LAST_NAME) as Author_Name,title_id as Title_ID, Title, sum(Profit) as Profit
+(select author_id as Author_ID, concat(a_s.first_name,' ',a_s.LAST_NAME) as Author_Name,title_id as Title_ID, Title, sum(sales_royalty)+advance as Profit
 
 from
-(select a.au_id as author_id, a.au_lname as LAST_NAME, a.au_fname as FIRST_NAME,t.title_id as title_id,t.title as title, pub.pub_name as pub_name,s.qty as saleqty,t.royalty,roy.royalty as roy2, ((t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100) + t.advance) as Profit
+(select a.au_id as author_id, a.au_lname as LAST_NAME, a.au_fname as FIRST_NAME,t.title_id as title_id,t.title as title, pub.pub_name as pub_name,s.qty as saleqty,t.royalty, (t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100) as sales_royalty, t.advance
 
 from authors as a
 left join titleauthor as ta
@@ -44,20 +44,15 @@ on roy.title_id = t.title_id
 );
 
 
-select Author_ID, sum(sales_royalty+advance) as Profit
-from
-temp3
-group by temp.author_id, temp.title_id
-order by profit desc
+
 ;
 
 
 -- Solution 2 (Create Table)
 
 create table most_profiting_authors
-as select temp3.author_id as au_id, sum(temp3.sales_royalty+temp3.advance) as profits
-from
-temp3
-group by temp3.author_id, temp3.title_id
+as select temp.author_id as au_id, sum(temp.sales_royalty)+advance as profits 
+from temp
+group by temp.author_id, temp.title_id, temp.advance
 order by profits desc
 ;
